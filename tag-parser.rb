d2da83705360f2d5fc51e6b697cf306d2a880bbb
Glen Maddern <glen83@gmail.com>
2009-12-01 15:45:31 +1100

    double quotes now being handled

--- tag-parser.rb
+++ tag-parser.rb
@@ -1,13 +1,26 @@
-
 class TagParser
   class Result < Struct.new(:tags, :errors)
   end
 
   def self.parse(input)
-    result = Result[input.split(" "),[]]
-
-    # YOUR CODE SHOULD GO HERE!
-    #
+    result = Result[[],[]]
+    str = input
+    while !str.empty?
+      str = consume(result, str)
+    end
     result
+  end
+  
+  #consume!
+  def self.consume result, str
+    if str =~ /^"/
+      matches = str.match(/^"([^"]+)"(.*)$/)
+      result.tags << matches.to_a[1]
+      return matches.to_a[2]
+    else
+      splits = str.split(" ")
+      result.tags << splits.first
+      return splits[1..-1].join(" ").strip
+    end
   end
 end

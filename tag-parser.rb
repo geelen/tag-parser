b1c01f535e334f00f1efae20b54e459a4434fed3
Glen Maddern <glen83@gmail.com>
2009-12-01 16:11:10 +1100

    handles single quotes too

--- tag-parser.rb
+++ tag-parser.rb
@@ -13,14 +13,15 @@ class TagParser
   
   #consume!
   def self.consume result, str
-    if str =~ /^"/
-      matches = str.match(/^"([^"]+)"(.*)$/)
-      result.tags << matches.to_a[1]
-      return matches.to_a[2]
-    else
-      splits = str.split(" ")
-      result.tags << splits.first
-      return splits[1..-1].join(" ").strip
-    end
+    %w[" '].each { |b|
+      if str =~ /^#{b}/
+        matches = str.match(/^#{b}([^#{b}]+)#{b}(.*)$/)
+        result.tags << matches.to_a[1]
+        return matches.to_a[2]
+      end
+    }
+    splits = str.split(" ")
+    result.tags << splits.first
+    return splits[1..-1].join(" ").strip
   end
 end

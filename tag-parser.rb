49d522536b52934b25af52e48dad253bc9006100
Glen Maddern <glen83@gmail.com>
2009-12-01 17:30:31 +1100

    Nicer checking of errors

--- tag-parser.rb
+++ tag-parser.rb
@@ -2,20 +2,33 @@ require 'patches'
 
 class TagParser
   class Result < Struct.new(:tags, :errors)
+    def self.errors errors
+      Result[[],errors]
+    end
   end
 
   def self.parse(input)
-    result = Result[[],[]]
+    input_errors = check_input input
+    if !input_errors.empty?
+      Result.errors input_errors
+    else
+      result = Result[[],[]]
+      str = input
+      while !str.empty?
+        str = consume(result, str)
+      end
+      result.tags.clear if !result.errors.empty?
+      result.tags = result.tags.uniq_by(&:downcase)
+      result
+    end
+  end
+  
+  def self.check_input input
     if input =~ /([^\w'"\.\- ])/
-      return Result[[],["The character '#{$1}' is not allowed in tag names"]]
+      ["The character '#{$1}' is not allowed in tag names"]
+    else
+      []
     end
-    str = input
-    while !str.empty?
-      str = consume(result, str)
-    end
-    result.tags.clear if !result.errors.empty?
-    result.tags = result.tags.uniq_by(&:downcase)
-    result
   end
   
   #consume!
